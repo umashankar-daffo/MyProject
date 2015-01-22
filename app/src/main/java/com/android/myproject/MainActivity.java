f7a199a32c2e2a85b6033b9e1ab6ec21d1f913b6
Uma Shanker <umashankar.gupta@daffodilsw.com>
2015-01-22 17:07:38 +0530

    error

--- app/src/main/java/com/android/myproject/MainActivity.java
+++ app/src/main/java/com/android/myproject/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Run Sucessfully");
+        System.out.println("print");
+
     }
 
 
