f4d2cf84c257fd3b34e7e471f5e40db1dc74d787
umashankar-daffo <umashankar.gupta@daffodilsw.com>
2015-01-22 17:46:05 +0530

    error

--- app/src/main/java/com/android/myproject/MainActivity.java
+++ app/src/main/java/com/android/myproject/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Run Sucessfully");
+        System.out.println("Run dsfdsfg");
         System.out.println("print");
 
     }
