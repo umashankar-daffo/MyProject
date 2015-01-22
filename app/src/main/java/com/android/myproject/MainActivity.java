e67162c5bcda16a1cafe0e806ddcfa37dac7760c
umashankar-daffo <umashankar.gupta@daffodilsw.com>
2015-01-22 17:50:51 +0530

    internal

--- app/src/main/java/com/android/myproject/MainActivity.java
+++ app/src/main/java/com/android/myproject/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Run dsfdsfg");
+        System.out.println("Run fghfghdhgfhd");
         System.out.println("print");
 
     }
