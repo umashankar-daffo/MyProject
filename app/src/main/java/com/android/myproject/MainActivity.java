f0a990c11f69fe67110615129f35d7b85aad5bc6
umashankar-daffo <umashankar.gupta@daffodilsw.com>
2015-01-22 19:07:10 +0530

    button added

--- app/src/main/java/com/android/myproject/MainActivity.java
+++ app/src/main/java/com/android/myproject/MainActivity.java
@@ -2,9 +2,14 @@ package com.android.myproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    String name[]  ={"Uma","Neeraj","Asha"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,8 +17,40 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         System.out.println("Run dsfdsfg");
         System.out.println("print");
+        Button b = (Button)findViewById(R.id.button);
+        b.setOnClickListener(this);
+        Button b2 = (Button)findViewById(R.id.button2);
+        b2.setOnClickListener(this);
+        Button b3 = (Button)findViewById(R.id.button3);
+        b3.setOnClickListener(this);
+        Button b4 = (Button)findViewById(R.id.button4);
+        b4.setOnClickListener(this);
 
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                Toast.makeText(this,name[0],Toast.LENGTH_LONG);
+                break;
+            case R.id.button2:
+                Toast.makeText(this,name[1],Toast.LENGTH_LONG);
+                break;
+            case R.id.button3:
+                Toast.makeText(this,name[2],Toast.LENGTH_LONG);
+                break;
+            case R.id.button4:
+                Toast.makeText(this,name[3],Toast.LENGTH_LONG);
+                break;
+
+
+
+
+
+        }
+
+
+    }
 }
